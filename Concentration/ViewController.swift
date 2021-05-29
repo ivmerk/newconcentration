dc8683075121c64807707036b82703db7171d39a
ivmerk <ivan.e.merkulov@gmail.com>
2021-05-29 21:47:41 +0300

    emoji include

--- Concentration/ViewController.swift
+++ Concentration/ViewController.swift
@@ -15,11 +15,22 @@ class ViewController: UIViewController {
         }
     }
     
+    func flipButton(emoji: String, button: UIButton){
+        if button.currentTitle == emoji {
+            button.setTitle("", for: .normal)
+            button.backgroundColor = #colorLiteral(red: 0.01680417731, green: 0.1983509958, blue: 1, alpha: 1)
+        } else {
+            button.setTitle(emoji, for: .normal)
+            button.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+        }
+    }
+    let emojiCollection = ["🦊", "🐰","🦊", "🐰"]
     
     @IBOutlet var buttonCollection: [UIButton]!
     @IBOutlet weak var touchLebel: UILabel!
     @IBAction func buttonAction(_ sender: UIButton) {
         touches += 1;
+        flipButton(emoji: emojiCollection[0], button: sender)
     }
 }
 
